9fb1fd85516c258f841174b1247119eccef96fe3
ggui1213 <yg2841@nyu.edu>
2025-04-06 18:11:57 -0400

    Push for Pair coding 4/6

--- Assets/Scripts/PlayerInputScript.cs
+++ Assets/Scripts/PlayerInputScript.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerInputScript : MonoBehaviour
 {
     public float moveDirection;
+    public float jumpDirection;
     [SerializeField] PlayerInput playerInput;
     void Awake()
     {
@@ -12,18 +13,33 @@ public class PlayerInputScript : MonoBehaviour
 
     private void OnEnable()
     {
+        //Move
         playerInput.actions["Move (En Garde)"].performed += OnMove;
         playerInput.actions["Move (En Garde)"].canceled += OnMove;
+        
+        //Jump
+        playerInput.actions["Jump"].performed += OnJump;
+        playerInput.actions["Jump"].canceled += OnJump;
     }
     
     private void OnDisable()
     {
+        //Move
         playerInput.actions["Move (En Garde)"].performed -= OnMove;
         playerInput.actions["Move (En Garde)"].canceled -= OnMove;
+        
+        //Jump
+        playerInput.actions["Jump"].performed -= OnJump;
+        playerInput.actions["Jump"].canceled -= OnJump;
     }
     
     private void OnMove(InputAction.CallbackContext context)
     {
         moveDirection = context.ReadValue<Vector2>().x;
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        jumpDirection = context.ReadValue<Vector2>().y;
     }
 }
