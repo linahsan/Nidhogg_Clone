a3dd09faa8cdea037bf2c96ed4da1e78c8425c15
CalebSnitch <70769047+CalebSnitch@users.noreply.github.com>
2025-04-13 12:27:18 -0400

    Added functioning input system

---
+++ Assets/Scripts/Player Input Test Script.cs
@@ -0,0 +1,48 @@
+//using System.Diagnostics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Video;
+
+public class PlayerInputTestScript : MonoBehaviour
+{
+    [SerializeField] PlayerInput playerInput;
+    public bool isPlayer1;
+    public string response;
+
+
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        /*
+        if (isPlayer1)
+        {
+            playerInput.defaultControlScheme = "PLAYER3";
+            Debug.Log("happened_1");
+        }
+        else
+        {
+            playerInput.defaultControlScheme = "Player2";
+            Debug.Log("happened");
+        }
+        Debug.Log(playerInput.currentControlScheme);
+        */
+    }
+
+    void OnEnable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInput.actions["Right"].triggered)
+        {
+            Debug.Log(response);
+            Debug.Log("happened");
+        }
+    }
+}
