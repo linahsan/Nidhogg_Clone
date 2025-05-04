dda4f3223fc4329b7341a401f173cdc0629a572e
CalebSnitch <70769047+CalebSnitch@users.noreply.github.com>
2025-05-03 21:29:53 -0400

    Cleaned Up Comments

--- Assets/Scripts/Camera Script.cs
+++ Assets/Scripts/Camera Script.cs
@@ -127,48 +127,10 @@ public class CameraScript : MonoBehaviour
             rightBorder.layer = LayerMask.NameToLayer("Ground");
             leftBorder.layer = LayerMask.NameToLayer("Ground");
 
-
-
-            /*
-
-            frameMaxX = maxX;
-            frameMinX = minX;
-            //set min
-            for(int i = 0; i < activePlayers.Count; i++)
-            {
-                if(activePlayers[i].GetComponent<Transform>().position.x - (barrierRange)*width > frameMinX)
-                {
-                    frameMinX = activePlayers[i].GetComponent<Transform>().position.x - (barrierRange)*width;
-                }
-            }
-
-            //set max
-
-            for(int i = 0; i < activePlayers.Count; i++)
-            {
-                if(activePlayers[i].GetComponent<Transform>().position.x - (barrierRange)*width < frameMaxX)
-                {
-                    frameMaxX = activePlayers[i].GetComponent<Transform>().position.x + (barrierRange)*width;
-                }
-            }
-            */
-            //Debug.Log("0");
         }
         else if(winningDirection == 1)
         {
-            /*
-            for(int i = 0; i < activePlayers.Count; i++)
-            {
-                if(!activePlayers[i].GetComponent<PlayerController>().isPlayer1)
-                {
-                    testPlayer = activePlayers[i];
-                }
-            }
-            */
-            
-
             frameMinX = player1Transform.position.x - (barrierRange)*width;
-            //frameMaxX = testPlayer.GetComponent<Transform>().position.x + (barrierRange)*width; 
             frameMaxX = player1Transform.position.x + (barrierRange)*width;
 
             if(player2Transform.position.x - (barrierRange)*width > frameMinX)
@@ -178,23 +140,9 @@ public class CameraScript : MonoBehaviour
 
             rightBorder.layer = LayerMask.NameToLayer("Player 1 Ignore");
             leftBorder.layer = LayerMask.NameToLayer("Player 2 Ignore");
-            //rightBorder.SetActive(false);
-            //leftBorder.SetActive(false);
-            //Debug.Log("1");
         }
         else if(winningDirection == -1)
         {
-            /*
-            for(int i = 0; i < activePlayers.Count; i++)
-            {
-                if(activePlayers[i].GetComponent<PlayerController>().isPlayer1)
-                {
-                    testPlayer = activePlayers[i];
-                }
-            }
-            */
-
-            //frameMinX = testPlayer.GetComponent<Transform>().position.x - (barrierRange)*width;
             frameMaxX = player2Transform.position.x + (barrierRange)*width; 
 
             frameMinX = player2Transform.position.x - (barrierRange)*width;
@@ -206,9 +154,6 @@ public class CameraScript : MonoBehaviour
 
             rightBorder.layer = LayerMask.NameToLayer("Player 1 Ignore");
             leftBorder.layer = LayerMask.NameToLayer("Player 2 Ignore");
-            //rightBorder.SetActive(false);
-            //leftBorder.SetActive(false);
-            //Debug.Log("-1");
         }
     }
 
