347cf64ccb66e2b633441c5547f4d2e747a193c7
linahsan <lina.sanchez.nyc@gmail.com>
2025-05-04 23:38:01 -0400

    slight updates to animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -249,7 +249,7 @@ public class PlayerController : MonoBehaviour
     void HandleCrouch()
     {
 
-        if (playerMovement.currentHeight == 0 && input.DownPressedLong() && isGrounded && !input.DownPressed())
+        if (playerMovement.currentHeight == 0 && input.DownPressedLong() && isGrounded && !input.DownPressed() && playerMovement.currentAnimation != "Armed_Standing_Idle_Low")
         {
 
             isCrouching = true;
