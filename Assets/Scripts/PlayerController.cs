b75f5c54395eb5f1d73137927ed490a89af5064e
ggui1213 <yg2841@nyu.edu>
2025-05-12 14:53:40 -0400

    Push for test

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -238,6 +238,7 @@ public class PlayerController : MonoBehaviour
     void HandleJump()
     {
         
+        
         if (input.JumpPressed() && isGrounded)
         {
             verticalVelocity = jumpForce;
